df1a4bf55bc934c52e05d5c4eaa7dd7e69ea2d46
Pavan Kalyan <pavankalyannidamanuri@gmail.com>
2024-02-11 10:31:42 +0530

    added filter function based on selected company

--- rtk-emart/src/stores/pages/MobilePage.js
+++ rtk-emart/src/stores/pages/MobilePage.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { mobileData } from '../data/mobiles';
-import { Button } from 'antd';
+import { Button,Checkbox } from 'antd';
 import Navbar from '../components/Navbar';
 import { Link } from 'react-router-dom';
 
 const MobilePage = () => {
+  const filtered=mobileData.filter((mobile,index)=>{
+    let ind=mobileData.findIndex(mb=>mb.id===mobile.id);
+    return ind===index;
+  })
+  const[selected,setSelected]=useState([]);
+  const handleSelected=(company)=>{
+      if(selected.includes(company))
+      {
+        setSelected(selected.filter(item=>item!==company));
+      }else{
+        setSelected([...selected,company])
+      }
+  }
+  const data=selected.length>0?mobileData.filter(mobile=>selected.includes(mobile.company)):mobileData;
   return (
     <>
     <Navbar/>
       <h2>Mobiles</h2>
+      
+      <div style={{display:"flex",justifyContent:"space-between",gap:"50px"}}>
+      <div>
+         {filtered.map(f=>(
+          <div>
+            <Checkbox checked={selected.includes(f.company)} onChange={()=>handleSelected(f.company)} >{f.company}</Checkbox>
+          </div>
+         ))}
+      </div>
         <div style={{display:"flex",flexWrap:"wrap",gap:"10px"}}>
-        {mobileData.map(mobile=>(
+        {data.map(mobile=>(
           <div style={{width:"30%"}}>
             <Link to={`/mobiles/${mobile.id}`}>
             <img src={mobile.image} alt="" style={{width:"80%",cursor:"pointer"}} />
@@ -20,6 +43,7 @@ const MobilePage = () => {
             <Button type='primary' danger>Remove</Button>
           </div>
         ))}
+        </div>
         </div>
     </>
   )
