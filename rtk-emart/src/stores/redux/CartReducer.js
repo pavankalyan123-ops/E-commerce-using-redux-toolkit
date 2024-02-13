861645514ae62e21062f491860946e6eec1b797a
Pavan Kalyan <pavankalyannidamanuri@gmail.com>
2024-02-13 15:07:13 +0530

    modified removeFromCart action

--- rtk-emart/src/stores/redux/CartReducer.js
+++ rtk-emart/src/stores/redux/CartReducer.js
@@ -19,8 +19,13 @@ const CartReducer=createSlice({
             if(index>-1)
             {
                 state.cartDetails.splice(index,1);
-                let priceArray=state.cartDetails.map(obj=>+obj.price);
+                if(state.cartDetails.length===0)
+                {
+                    state.totalPrice=0;
+                }else{
+                    let priceArray=state.cartDetails.map(obj=>+obj.price);
             state.totalPrice=priceArray.reduce((a,b)=>a+b);
+                }
             }else{
                 alert("you have not added this product into your cart")
             }
